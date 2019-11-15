34022e13218840970b381bcd7f2ee80dcd6225f7
EliasAachach <elaachac@student.42.fr>
2019-11-15 16:02:09 +0100

    fixes 3.1

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,26 +6,22 @@
 /*   By: elaachac <elaachac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/14 16:27:33 by elaachac          #+#    #+#             */
-/*   Updated: 2019/11/14 17:18:20 by elaachac         ###   ########.fr       */
+/*   Updated: 2019/11/15 16:01:57 by elaachac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    ft_putchar (char c, int fd)
-{
-    write(fd, &c, 1);
-}
 void    ft_putnbr_fd(int n, int fd)
 {
     unsigned int nbr;
     nbr = n;
     if (n < 0)
     {
-        ft_putchar('-', fd);
+        ft_putchar_fd('-', fd);
         nbr = -n;
     }
     if (nbr >= 10)
         ft_putnbr_fd(nbr / 10, fd);
-    ft_putchar(nbr % 10 + '0', fd);
+    ft_putchar_fd(nbr % 10 + '0', fd);
 }
