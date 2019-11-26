b1e4929b899598ccd85b7056e36918a3e6b067f5
EliasAachach <elaachac@student.42.fr>
2019-11-26 02:07:24 +0100

    fixes 11

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -14,24 +14,20 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	len;
-	char	*str;
-	int		i;
+	char			*dest;
+	unsigned int	i;
 
 	i = 0;
-	len = ft_strlen(s1) + ft_strlen(s2);
-	if (!(str = (char *)malloc(sizeof(char) * len + 1)))
+	if (s1 == NULL || s2 == NULL)
 		return (NULL);
-	while (s1)
-	{
-		str[i] = *s1++;
-		i++;
-	}
-	while (s2)
-	{
-		str[i] = *s2++;
-		i++;
-	}
-	str[i] = '\0';
-	return (str);
+	dest = (char*)malloc(sizeof(*dest) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (dest == NULL)
+		return (NULL);
+	while (*s1 != '\0')
+		dest[i++] = *s1++;
+	while (*s2 != '\0')
+		dest[i++] = *s2++;
+	dest[i] = '\0';
+	return (dest);
 }
+
