de653c36dfc8f4e1c8dc4b64b6886e93743bccef
Elias AACHACH <elaachac@e3r3p17.clusters.42paris.fr>
2021-10-26 15:12:40 +0200

    norm strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,19 +6,20 @@
 /*   By: elaachac <elaachac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/06 16:16:51 by elaachac          #+#    #+#             */
-/*   Updated: 2019/11/29 16:04:08 by elaachac         ###   ########.fr       */
+/*   Updated: 2021/10/26 15:11:58 by elaachac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strdup(const char *s1)
+char	*ft_strdup(char *s1)
 {
 	size_t	i;
 	char	*s2;
 
 	i = 0;
-	if (!(s2 = ((char *)malloc(sizeof(char) * (ft_strlen(s1) + 1)))))
+	s2 = ((char *)malloc(sizeof(char) * (ft_strlen(s1) + 1)));
+	if (!s2)
 		return (NULL);
 	while (s1[i])
 	{
